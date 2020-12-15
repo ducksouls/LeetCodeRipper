61989ee7192ca482ab1fa1552a75fedb1f75561f
ducksouls <879075992@qq.com>
2020-12-15 16:10:00 +0800

    ---办公室恋情

---
+++ src/app/NonDecreasingArray.java
@@ -0,0 +1,24 @@
+package app;
+
+public class NonDecreasingArray {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = { 4, 1, 2 };
+        System.out.println(solution.checkPossibility(nums));
+
+    }
+}
+
+class Solution {
+    /**
+     * 定义递减数组 nums[i] <= nums[i+1] (0 <= i <= n-2)
+     * 
+     * @param nums
+     * @return
+     */
+    public boolean checkPossibility(int[] nums) {
+        if (num)
+            return false;
+    }
+}
